4fb81ce01e9db4d0e87d5da8e29fb3e145c22428
sakurawa <root@sakuraawa.dev>
2024-07-08 15:38:20 +0800

    feat: implement

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,37 @@
+import { Redis } from '@upstash/redis/cloudflare';
+
 export default {
-  async fetch(request, env, ctx) {
-	  const url = new URL(request.url);
-    if (url.pathname === '/')
-      return new Response(JSON.stringify({
-        code: 200,
-        message: "It's SakuraKooi's Blog PV counter here!"
-      }));
-      return new Response(JSON.stringify({
-        code: 200,
-        message: "It's SakuraKooi's Blog PV counter here!"
-      }));
-  },
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+
+		let key = url.pathname.substring(1);
+		if (key === '')
+			return new Response(JSON.stringify({
+				code: 200,
+				message: 'It\'s SakuraKooi\'s Blog PV counter here!'
+			}));
+
+		if (request.headers.get('Sakura-Access-Token') !== env['ACCESS_TOKEN'])
+			return new Response(JSON.stringify({
+				code: 403,
+				message: 'Access denied'
+			}));
+
+		const redis = Redis.fromEnv(env);
+
+		let visitTrackKey = `t_${key}_${request.headers.get('CF-Connecting-IP')}`;
+		let counterKey = `c_${key}`;
+		let current;
+		if (await redis.get(visitTrackKey) != null) {
+			current = await redis.get(counterKey);
+		} else {
+			await redis.setex(visitTrackKey, 60 * 30, 1);
+			current = await redis.incr(counterKey);
+		}
+		return new Response(JSON.stringify({
+			code: 100,
+			message: 'ok',
+			counter: current
+		}));
+	}
 };
